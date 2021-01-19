6bd1d9d86595907b5a70d4cd359c81d3e9b190c9
Tomás Jiménez <tjimenez999@gmail.com>
2021-01-19 03:08:58 -0300

    first commit

---
+++ src/sensor-ufro_node.cpp
@@ -0,0 +1,54 @@
+
+#include <ros/ros.h>
+#include <sensor_msgs/BatteryState.h>
+
+#include <iostream>
+#include <fstream>
+#include "sqlite3.h"
+
+sensor_msgs::BatteryState battery_state_;
+
+void batteryStateCallback(const sensor_msgs::BatteryState::ConstPtr& batteryState) {
+    battery_state_ = *batteryState;
+    ROS_INFO_STREAM("Voltaje bateria: " << (*batteryState).voltage, ", Carga de bateria: " << (*batteryState).charge);
+}
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+ros::Subscriber batteryStateSub;
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "sensor-ufro_node");
+    sqlite3 *db_handle;
+    char *sql;
+    char * errorMsg = 0;
+
+    sqlite3_open("basedatos.db", &db_handle);
+    ROS_INFO_STREAM("Se abrió la base de datos");
+    sql = "CREATE TABLE DATOS("  \
+      "ID INT PRIMARY KEY     NOT NULL," \
+      "VALOR           TEXT    NOT NULL," \
+      "FECHA           TEXT     NOT NULL);";
+
+    int exec = sqlite3_exec(db_handle, sql, callback, 0, &errorMsg);
+    if( exec != SQLITE_OK ){
+      ROS_INFO_STREAM(errorMsg);
+      sqlite3_free(errorMsg);
+   } else {
+      ROS_INFO_STREAM("Tabla creada correctamente");
+   }
+   
+    ros::NodeHandle nh;
+    batteryStateSub = nh.subscribe("dji_osdk_ros/battery_state",10,&batteryStateCallback);
+    if (!ros::ok()) sqlite3_close(db_handle);
+    ros::spin();
+    return 0;
+
+}
